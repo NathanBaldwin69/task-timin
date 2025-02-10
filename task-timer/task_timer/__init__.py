05975e1c0da02ce22841df1c68ee8ecfad966519
NathanBaldwin69 <nathan.baldwin@student.cune.edu>
2025-02-10 01:22:42 -0600

    Update __init__.py

--- task-timer/task_timer/__init__.py
+++ task-timer/task_timer/__init__.py
@@ -1,1 +1,15 @@
+"""Task Timer Package Initialization.
+
+This module initializes the Task Timer package and provides access to the main function.
+
+The Task Timer application allows users to track time spent on tasks, start and stop tasks, 
+view summaries, edit timesheets, and check currently running tasks.
+
+Modules:
+- __main__: Contains the main function to run the Task Timer application.
+
+Author: Nathan Baldwin
+Date: February 9, 2025
+"""
+
 from task_timer.__main__ import main
